2e5c894c2815382ef4eed04accab97091bbd5da7
lmo0317 <lmo0317@naver.com>
2016-08-18 22:15:18 +0900

    테스트

--- schema/trading.js
+++ schema/trading.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 exports.TradingSchema = new mongoose.Schema({
-    isu_cd: {type:String, require:true}, //종목 코드
-    date: {type:Date, require:true} //날짜와 시간
+    stocklist: {type:Array, require:true}, //종목 코드
+    date: {type:String, require:true} //날짜와 시간
 });
