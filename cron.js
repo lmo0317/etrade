ac1cf20e29f8c3905c5d15a77d1d2fc9b46c7a43
lmo0317 <lmo0317@naver.com>
2016-08-28 04:22:06 +0900

    start

---
+++ cron.js
@@ -0,0 +1,40 @@
+/**
+ * Created by LEE-DESKTOP on 2016-08-26.
+ */
+
+var cronJob = require('cron').CronJob;
+var fs = require('fs')
+var sync = require('synchronize');
+var moment = require('moment');
+var tradinglib = require('./lib/trading');
+var yaml = require('yamljs');
+var mongoose = require('mongoose');
+var util = require('./lib/util');
+
+var cronSchedule = '0 * * * * *';
+console.log(cronSchedule);
+
+global.configure = yaml.load('./default.config.yml')
+global.DB = {};
+global.program = require('commander');
+global.program
+    .version('0.0.1')
+    .option('--d, --develop', 'Develop')
+    .parse(process.argv);
+
+//db
+mongoose.connect(global.configure.db.path);
+
+new cronJob(cronSchedule, function(){
+
+    console.log('cron schedule', moment().format("YYYYMMDDHHmm"));
+    sync.fiber(function() {
+
+        sync.await(tradinglib.findTrading(sync.defer()));
+
+    }, function(err, result) {
+        if(err) return console.log(err);
+        console.log('complete find trading');
+    });
+
+},null, true, 'Asia/Seoul');
--- schema/trading.js
+++ schema/trading.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 exports.TradingSchema = new mongoose.Schema({
     date: {type:String, require:true},
     isu_nm: {type:String, require:true},
-    isu_cd: {type:String, require:true},
-    trade: {type:Array}
+    isu_srt_cd: {type:String, require:true},
+    trade:  {type: mongoose.Schema.Types.Mixed}
 });
