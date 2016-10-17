4e10d788cd4bba20484f47042a8819929433c4c3
lmo0317 <lmo0317@naver.com>
2016-10-17 21:06:08 +0900

    [FEATURE] 매도 기준 내리차순 정리

--- main.js
+++ main.js
@@ -2,15 +2,11 @@
  * Created by LEE-DESKTOP on 2016-08-26.
  */
 
-var cronJob = require('cron').CronJob;
-var fs = require('fs')
 var sync = require('synchronize');
 var moment = require('moment');
 var tradinglib = require('./lib/trading');
 var stocklistlib = require('./lib/stocklist');
-var yaml = require('yamljs');
 var mongoose = require('mongoose');
-var util = require('./lib/util');
 var config = require('./config');
 
 config.init();
