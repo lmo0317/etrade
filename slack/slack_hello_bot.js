406d2715d908d4f27b7b7c0bb88ccb2f62e6823b
lmo0317 <lmo0317@naver.com>
2016-10-18 02:11:33 +0900

    [FEATURE] [SLACK BOT] hello bot 추가

---
+++ slack/slack_hello_bot.js
@@ -0,0 +1,20 @@
+/**
+ * Created by LEE-DESKTOP on 2016-10-18.
+ */
+
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+
+var bot = controller.spawn({
+    token: "xoxb-92380329216-lQWNgEMOOFbaoM6UzgakzaFn"
+});
+
+bot.startRTM(function(err,bot,payload) {
+    if (err) {
+        throw new Error('Could not connect to Slack');
+    }
+});
+
+controller.hears(["Hello","Hi"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+    bot.reply(message,'Hello, how are you today?');
+});
