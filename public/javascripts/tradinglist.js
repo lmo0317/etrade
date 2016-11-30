303db04a283ee433784f6a1214deafce4e2e7e1d
lmo0317 <lmo0317@naver.com>
2016-12-01 00:40:51 +0900

    [FEATURE] delete 버튼 추가

--- public/javascripts/tradinglist.js
+++ public/javascripts/tradinglist.js
@@ -52,8 +52,7 @@ function makeTradeTableProcess(data)
     $("#tbody_trading_container").html('');
 
     var parameter = {
-        exception: $('input:checkbox[name=check_box_exception_top]').is(':checked'),
-        edit_start: $("#edit_start").val()
+        exception: $('input:checkbox[name=check_box_exception_top]').is(':checked')
     };
 
     makeTradeTable(parameter, data);
@@ -88,36 +87,39 @@ function addButton() {
 
 function makeTradeTable(parameter, data) {
 
-    data.forEach(function(value) {
+    data.forEach(function(stock) {
 
         var tr = $("<tr>").attr("id", "tr_trading_list");
         var td_name = $("<td>").attr("id", "td_name");
-        td_name.text(value.isu_nm);
+        td_name.text(stock.isu_nm);
 
         var td_trading_updn_rate = $("<td>").attr("id", "td_trading_updn_rate");
-        var stockInfo = value.buylist[value.buylist.length - 1].stockinfo;
+        var stockInfo = stock.buylist[stock.buylist.length - 1].stockinfo;
         var updn_rate = numberWithCommas((stockInfo && stockInfo.updn_rate) || 0);
         td_trading_updn_rate.text(updn_rate);
 
         var td_trading_netaskval = $("<td>").attr("id", "td_trading_netaskval");
         var netaskval = 0;
         if( parameter.exception ) {
-            netaskval = numberWithCommas(value.buylist[value.buylist.length - 1].netaskvalhidden);
+            netaskval = numberWithCommas(stock.buylist[stock.buylist.length - 1].netaskvalhidden);
         } else {
-            netaskval = numberWithCommas(value.buylist[value.buylist.length - 1].netaskval);
+            netaskval = numberWithCommas(stock.buylist[stock.buylist.length - 1].netaskval);
         }
         td_trading_netaskval.text(netaskval);
 
         var td_button = $("<td>").attr("id", "td_button");
 
         //detail 버튼 추가
-        createDetailButton(parameter, value, td_button);
+        createDetailButton(stock, td_button);
 
-        //edit 버튽 추가
-        createEditButton(parameter, value, td_button);
+        //edit 버튼 추가
+        createEditButton(stock, td_button);
+        
+        //delete 버튼 추가
+        createDeleteButton(stock, td_button);
 
         var td_grade = $("<td>").attr("id", "td_grade");
-        td_grade.text(value.grade || 0);
+        td_grade.text(stock.grade || 0);
 
         tr.append(td_name);
         tr.append(td_trading_updn_rate);
@@ -128,21 +130,21 @@ function makeTradeTable(parameter, data) {
     });
 }
 
-function createDeleteButton(parameter, value, td_button)
+function createDeleteButton(stock, td_button)
 {
     var button_delete = $("<input>")
         .attr("type", "button")
         .attr("id", "btn_edit" )
-        .attr("class", "btn btn-primary")
+        .attr("class", "btn btn-danger")
         .attr("style", "margin-left: 10px")
-        .val('EDIT');
+        .val('DELETE');
     button_delete.click(function() {
-        clickEditButton(parameter, value);
+        clickDeleteButton(stock);
     });
     td_button.append(button_delete);
 }
 
-function createEditButton(parameter, value, td_button)
+function createEditButton(stock, td_button)
 {
     var button_edit = $("<input>")
         .attr("type", "button")
@@ -151,12 +153,12 @@ function createEditButton(parameter, value, td_button)
         .attr("style", "margin-left: 10px")
         .val('EDIT');
     button_edit.click(function() {
-        clickEditButton(parameter, value);
+        clickEditButton(stock);
     });
     td_button.append(button_edit);
 }
 
-function createDetailButton(parameter, value, td_button)
+function createDetailButton(stock, td_button)
 {
     var button_detail = $("<input>")
         .attr("type", "button")
@@ -164,16 +166,39 @@ function createDetailButton(parameter, value, td_button)
         .attr("class", "btn btn-primary")
         .val('DETAIL');
     button_detail.click(function() {
-        clickDetailButton(parameter, value);
+        clickDetailButton(stock);
     });
     td_button.append(button_detail);
 }
 
-function clickDeleteButton(parameter, stock) {
-
+function deleteStock(stock, callback) {
+    $.ajax({
+        url: '/trading',
+        type: 'put',
+        data: {
+            start: $("#edit_start").val(),
+            isu_nm: stock.isu_nm,
+            grade: 3
+        },
+        success: function (data) {
+            alert('complete edit trading');
+            callback();
+        },
+        error: function (err) {
+            console.log(err);
+            alert(err.responseText);
+            callback();
+        }
+    });
 }
 
-function clickEditButton(parameter, stock) {
+function clickDeleteButton(stock) {
+    deleteStock(stock, function() {
+        getTradingList();
+    });
+}
+
+function clickEditButton(stock) {
     if(stock.buylist.length <= 0) return;
     var pop = window.open('edittrading.html');
     pop.onload = function() {
@@ -184,11 +209,11 @@ function clickEditButton(parameter, stock) {
         isu_nm.val(stock.isu_nm);
 
         var edit_start = $(pop.document).find("#edit_start");
-        edit_start.val(parameter.edit_start);
+        edit_start.val($("#edit_start").val());
     };
 }
 
-function clickDetailButton(parameter, stock) {
+function clickDetailButton(stock) {
     if(stock.buylist.length <= 0) return;
     var pop = window.open('detailtrading.html');
     pop.onload = function() {
@@ -199,6 +224,6 @@ function clickDetailButton(parameter, stock) {
         isu_nm.val(stock.isu_nm);
 
         var edit_start = $(pop.document).find("#edit_start");
-        edit_start.val(parameter.edit_start);
+        edit_start.val($("#edit_start").val());
     };
 }
