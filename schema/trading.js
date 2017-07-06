22b7506cca9bb64535240a5c21171fdff2c5b6a5
lmo0317 <lmo0317@naver.com>
2017-07-06 23:14:49 +0900

    [FEATURE] 순매수 등락률 계산 수정

--- schema/trading.js
+++ schema/trading.js
@@ -5,7 +5,7 @@ exports.TradingSchema = new mongoose.Schema({
     isu_nm: {type:String, require:true},
     isu_srt_cd: {type:String, require:true},
     grade: {type: Number, require:true, default:3},
-    remove: {type: Boolean, require:true, default:false},
+    removed: {type: Boolean, require:true, default:false},
     buylist: {type: Array},
     memberlist: {type: Array}
 }, {versionKey: false});
